b34ca1e39dfb792154d002acb4bb4aec622b3e8b
Thiago Sales <thiago.ss133@gmail.com>
2020-02-27 02:14:33 -0300

    Tasks Backend, Navigator e Login

---
+++ tasks/src/screens/AddTask.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react'
+import { Modal, View, StyleSheet,
+    TouchableWithoutFeedback,
+    Text,
+    TouchableOpacity,
+    TextInput, 
+    Platform
+} from 'react-native'
+import commonStyles from '../CommomStyles'
+import DateTimePicker from '@react-native-community/datetimepicker'
+import moment from 'moment'
+
+const initialState = {desc: '', date: new Date(), showDatePicker: false }
+
+export default class AddTask extends Component {
+
+    state = {
+        ...initialState
+    }
+
+    save = () => {
+        const newTask = {
+            desc: this.state.desc,
+            date: this.state.date
+        }
+
+        this.props.onSave && this.props.onSave(newTask);
+        this.setState({ ...initialState })
+    }
+
+    getDatePicker = () => {
+        let datePicker =  <DateTimePicker 
+            value={this.state.date}
+            onChange={(_, date) => this.setState({ date, showDatePicker: false })} 
+            mode='date' />;
+
+
+        const dateString = moment(this.state.date).format('ddd, D [de] MMMM [de] YYYY')
+
+        if(Platform.OS === 'android') {
+            datePicker = (
+                <View>
+                    <TouchableOpacity onPress={() => this.setState({ showDatePicker: true})}>
+                        <Text style={styles.date}>
+                            {dateString}
+                        </Text>
+                    </TouchableOpacity>
+                    {this.state.showDatePicker && datePicker}
+                </View>
+            )
+        }
+
+        return datePicker
+    }
+    
+    render() {
+        return (
+            <Modal transparent={true} visible={this.props.isVisible} 
+                onRequestClose={this.props.onCancel} animationType='slide'>
+                <TouchableWithoutFeedback 
+                    onPress={this.props.onCancel}>
+                    <View style={styles.overlay} />
+                </TouchableWithoutFeedback>
+
+                <View style={styles.container}>
+                    <Text style={styles.header}>Nova Tarefa</Text>
+                    <TextInput style={styles.input} 
+                        placeholder="Informe a descrição!"
+                        onChangeText={desc => this.setState({desc})}
+                        value={this.state.desc}/>
+                    {this.getDatePicker()}
+                    <View style={styles.buttons}>
+                        <TouchableOpacity onPress={this.props.onCancel}>
+                            <Text style={styles.button}>Cancelar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={this.save}>
+                            <Text style={styles.button}>Salvar</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+
+                <TouchableWithoutFeedback 
+                    onPress={this.props.onCancel}>
+                    <View style={styles.overlay} />
+                </TouchableWithoutFeedback>
+            </Modal>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    overlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.7)'
+    },
+    container: {
+       // flex: 2,
+        backgroundColor: '#FFF'
+    },
+    header: {
+        fontFamily: commonStyles.fontFamily,
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secundary,
+        textAlign: 'center',
+        padding: 15,
+        fontSize: 18
+    },
+    buttons:{
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    input: {
+        fontFamily: commonStyles.fontFamily,
+        height: 40,
+        marginRight: 15,
+        marginTop: 10,
+        marginLeft: 10,
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#E3E3E3',
+        borderRadius: 6
+        
+    },
+    button: {
+        margin: 20,
+        marginRight: 30,
+        color: commonStyles.colors.today
+    },
+    date: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 20,
+        marginLeft: 15
+    }
+});
